16dd940cfa68a531393bea3b57cb5414f53b3b93
Jackey-JQ <q23jia@uwaterloo.ca>
2016-09-24 12:50:21 -0400

    Two ways binding

--- myApp.js
+++ myApp.js
@@ -1,13 +1,4 @@
 angular.module("myapp", [])
-
 .controller("MyController", function ($scope) {
-  var person = {
-    firstName:"Jimmy",
-    age: 21,
-    address:{
-      street: '16 Somewhere Drive',
-      suburb: 'Port Kennedy',
-      state:  'Western Australia'
-} }
-  $scope.person = person;
-});
+$scope.showFirstName = function(){ alert("Name is: " + $scope.firstName);
+} });
